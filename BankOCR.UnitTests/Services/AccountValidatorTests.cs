4a37977918cdea78bc0b888039b2b9193f5a884a
Arkadiusz Neuman <a.neuman@cognitum.eu>
2021-03-09 20:13:58 +0100

    add UserStory1 account validator

---
+++ BankOCR.UnitTests/Services/AccountValidatorTests.cs
@@ -0,0 +1,17 @@
+﻿using BankOCR.Services;
+using NUnit.Framework;
+
+namespace BankOcr.UnitTests.Services
+{
+    public class AccountValidatorTests : BaseUnitTest<AccountValidator>
+    {
+        [TestCase("123456789", ExpectedResult = true)]
+        [TestCase("012345678", ExpectedResult = true)]
+        [TestCase("01234567", ExpectedResult = false)]
+        [TestCase("0123456789", ExpectedResult = false)]
+        public bool IsAccountValid_AccountNumber_CheckIsValid(string accountNumber)
+        {
+            return Sut.IsAccountValid(accountNumber);
+        }
+    }
+}
---
+++ BankOCR/Services/AccountValidator.cs
@@ -0,0 +1,13 @@
+﻿namespace BankOCR.Services
+{
+    public class AccountValidator
+    {
+        public bool IsAccountValid(string accountNumber)
+        {
+            if (accountNumber.Length != 9)
+                return false;
+            
+            return true;
+        }
+    }
+}
